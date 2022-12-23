1398dcc755af984a2852ea2a080c6baeb40b2037
nothingmatters <n0th1ngmatters@proton.me>
2022-12-23 19:51:05 +0100

    corrections and updates

--- src/utils/renderOptions.js
+++ src/utils/renderOptions.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const renderOptions = (images) => {
-    images = images.edges;
+    images = images.edges ? images.edge : images;
     return {
         renderNode : {
           "embedded-asset-block":(node)=> {
